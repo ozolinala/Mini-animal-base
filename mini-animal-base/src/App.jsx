c75f83e50c112c4d40dd3ae8ea2a17e5be6f3ce2
Laura Ozolina <laur755v@stud.kea.dk>
2023-04-17 23:29:07 +0200

    sorting works + added unique key to tbody tr

--- mini-animal-base/src/App.jsx
+++ mini-animal-base/src/App.jsx
@@ -18,11 +18,35 @@ import './App.css'
 function App() {
 const animals = cleanUpData();
 const [filter, setFilter] = useState("")
-console.log(animals);
+const [sortKey, setSortKey] = useState("");
+const [sortDirection, setSortDirection] = useState("desc");
+
 let filteredAnimals = [...animals];
 
 if(filter){
   filteredAnimals = filteredAnimals.filter(ani=>ani.type===filter)
+}
+
+if(sortKey){
+  filteredAnimals.sort((a, b) => {
+    if (a[sortKey] < b[sortKey]){
+      return sortDirection === "asc" ? -1 : 1;
+    }
+    if(a[sortKey]>b[sortKey]){
+      return sortDirection === "asc" ? 1 : -1;
+    }
+    //names must be equal
+    return 0;
+  })
+}
+
+function setSorting(key) {
+  if(key === sortKey){
+    setSortDirection(oldState => oldState==="asc" ? "desc":"asc");
+  } else {
+    setSortDirection("asc");
+  }
+  setSortKey(key);
 }
 
   return (
@@ -35,15 +59,15 @@ if(filter){
       <table>
         <thead>
           <tr>
-            <td>Name</td>
-            <td>Trait</td>
-            <td>Type</td>
-            <td>Age</td>
+            <td><button onClick={()=>setSorting("name")}>Name</button> </td>
+            <td><button onClick={()=>setSorting("trait")}>Trait</button> </td>
+            <td><button onClick={()=>setSorting("type")}>Type</button> </td>
+            <td><button onClick={()=>setSorting("age")}>Age</button> </td>
           </tr>
         </thead>
         <tbody>
           {filteredAnimals.map((animal) => (
-            <tr>
+            <tr key={animal.name}>
               <td>{animal.name}</td>
               <td>{animal.trait}</td>
               <td>{animal.type}</td>
