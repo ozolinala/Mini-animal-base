fc4daae7321cbd88467d58c1adb801a68bc5ea3e
Laura Ozolina <laur755v@stud.kea.dk>
2023-04-17 22:46:38 +0200

    structure for the table

--- mini-animal-base/src/App.jsx
+++ mini-animal-base/src/App.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react'
+import data from '../animals.json';
+console.log(data);
+function cleanUpData(){
+  return data.map((animal) => {
+    const  [name, , trait, type] = animal.fullname.split(" ");
+    return {
+      name, 
+      trait,
+      type,
+      age: animal.age,
+    };
+  });
+}
+
 import './App.css'
 
 function App() {
-
+const animals = cleanUpData();
+console.log(animals);
 
   return (
     <div className="App">
       <h1>Mini Animal Base </h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Trait</td>
+            <td>Type</td>
+            <td>Age</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            animals.map((animal) => (
+            <tr>
+              <td>{animal.name}</td>
+              <td>{animal.trait}</td>
+              <td>{animal.type}</td>
+              <td>{animal.age}</td>
+            </tr>  
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
 
-export default App
+export default App;
